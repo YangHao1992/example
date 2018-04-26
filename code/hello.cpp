939360d99ecb9e8fcb69935b4dab0ea205aade7a
YangHao1992 <18611856983@163.com>
2018-04-26 17:12:42 +0800

    test

--- code/hello.cpp
+++ code/hello.cpp
@@ -2,4 +2,7 @@
 int main() {
     std::cout << "HelloWorld!" << std::endl;
     std::cout << "add some info in github." << std::endl;
+	char c = '8';
+	while(1) {}
+	std::cout << "result = " << c << std::endl;
 }
